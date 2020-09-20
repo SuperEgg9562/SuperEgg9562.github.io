ab141f7aa6a2aa3c4953341e79bda8a01782146e
Joseph Marks <tentaclefish@gmail.com>
2020-09-21 10:56:02 +1200

    Update Countdownscript.js

--- Countdownscript.js
+++ Countdownscript.js
@@ -1,29 +1,16 @@
-
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-
-var myfuncCT = setInterval(function() {
-
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("days").innerHTML = days + "d ";
-  document.getElementById("hours").innerHTML = hours + "h ";
-  document.getElementById("mins").innerHTML = minutes + "m ";
-  document.getElementById("secs").innerHTML = seconds + "s";
-
-  if (timeleft < 0) {
-    clearInterval(myfuncCT);
-    document.getElementById("days").innerHTML = "";
-    document.getElementById("hours").innerHTML = "";
-    document.getElementById("mins").innerHTML = "";
-    document.getElementById("secs").innerHTML = "";
-    document.getElementById("end").innerHTML = "Expired ";  
-    
-  }
-}, 1000);
+var countDownDate = new Date("June 5, 2022 11:27:15").getTime();
+   var timeClear = setInterval(function() {
+      var now = new Date().getTime();
+      var timeLeft = countDownDate - now;
+      var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+         (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+      document.querySelector(".timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      if (timeLeft < 0) {
+         clearInterval(timeClear);
+         document.querySelector(".timer").innerHTML = "Timer Finished";
+      }
+   }, 1000);
